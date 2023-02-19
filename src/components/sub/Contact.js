df7dc75405d800fd14f48fff0ccf4d03663aa58d
Shin-Yumi <tlsdbal0517@naver.com>
2023-02-19 23:13:30 +0900

    emailjs script 추가

--- src/components/sub/Contact.js
+++ src/components/sub/Contact.js
@@ -14,12 +14,12 @@ function Contact() {
 	const sendEmail = (e) => {
 		e.preventDefault();
 
-		emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY').then(
+		emailjs.sendForm("service_a67ezzs", "template_g0rfbw9", form.current, "smXrTUT7qtrtDtUhx").then(
 			(result) => {
-				console.log(result.text);
+				alert("전송되었습니다.");
 			},
 			(error) => {
-				console.log(error.text);
+				alert("전송을 실패했습니다.");
 			}
 		);
 	};
@@ -28,23 +28,23 @@ function Contact() {
 			<section id='contact'>
 				<div className='contactImg'></div>
 				<div className='contactBox'>
-					<form method='post' id='contactForm'>
+					<form ref={form} onSubmit={sendEmail} id='contactForm'>
 						<div className='formBox'>
 							<label htmlFor='contactName'>Your name</label>
-							<input name='contactName' id='contactName' type='text' placeholder='name' />
+							<input name='name' id='contactName' type='text' placeholder='name' />
 						</div>
 						<div className='formBox'>
 							<label htmlFor='contactName'>Your Phone</label>
-							<input name='contactPhone' id='contactPhone' type='text' placeholder='Phone-number' />
+							<input name='phone' id='contactPhone' type='text' placeholder='Phone-number' />
 						</div>
 						<div className='formBox'>
 							<label htmlFor='contactName'>Your Email</label>
-							<input name='contactEmail' id='contactEmail' type='email' placeholder='Email' />
+							<input name='email' id='contactEmail' type='email' placeholder='Email' />
 						</div>
 						<div className='formBox'>
 							<label htmlFor='contactName'>Your Message</label>
 							<textarea
-								name='contactMsg'
+								name='message'
 								id='contactMsg'
 								cols='5'
 								rows='5'
@@ -52,7 +52,7 @@ function Contact() {
 							></textarea>
 						</div>
 						<div className='formBox'>
-							<input className='submit' value='submit' type='submit' />
+							<input className='submit' value="Send" type='submit' />
 						</div>
 					</form>
 				</div>
