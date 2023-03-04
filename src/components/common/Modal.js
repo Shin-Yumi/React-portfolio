aa23037054faa0cbe835bd3f1e6409dc2d013d4d
Shin-Yumi <tlsdbal0517@naver.com>
2023-03-04 22:37:39 +0900

    modal.js motion add

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -21,11 +21,18 @@ const Modal = forwardRef((props, ref) => {
 					animate={{ opacity: 1, scale: 1, transition: { duration: 0.6 } }}
 					exit={{ opacity: 0, transition: { duration: 0.6 } }}
 				>
-					<div className='con'>{props.children}</div>
-					<span className='close' onClick={() => setOpen(false)}>
+					<motion.div
+						className='con'
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1, transition: { delay: 0.6 } }}
+						exit={{ opacity: 0 }}
+					>
+						{props.children}
+					</motion.div>
+					<span className='close' onClick={() => setOpen(false)} >
 						Close
 					</span>
-				</motion.aside> 
+				</motion.aside>
 			)}
 		</AnimatePresence>
 	);
