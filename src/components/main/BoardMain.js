586b325a6cf7cd8ca653f11ce812c24cae13decf
Shin-Yumi <tlsdbal0517@naver.com>
2023-03-11 23:33:28 +0900

    boardMain layout 변경 pc적용

--- src/components/main/BoardMain.js
+++ src/components/main/BoardMain.js
@@ -2,20 +2,6 @@ import { useState, useEffect, useRef, useCallback, memo } from 'react';
 import { Link } from 'react-router-dom';
 
 function BoardMain() {
-	const board = useRef(null);
-
-	const typing = useCallback(() => {
-		const textBox = board.current.querySelector('.boardTextArea');
-		const boardTop = board.current.offsetTop - 80;
-		const scroll = window.scrollY;
-		const base = -window.innerHeight / 3;
-
-		window.addEventListener('scroll', () => {
-			if (scroll >= boardTop + base) textBox.classList.add('on');
-			else textBox.classList.remove('on');
-		});
-	},[]);
-
 	const getLocalData = () => {
 		const dummys = [
 			{ title: 'Hello3', content: 'Here comes description in detail.' },
@@ -34,60 +20,50 @@ function BoardMain() {
 		localStorage.setItem('post', JSON.stringify(Posts));
 	}, [Posts]);
 
-	useEffect(() => {
-		typing();
-	}, [typing]);
-
 	return (
-		<section className='contents boardCont myScroll' ref={board}>
+		<section className='contents boardCont myScroll'>
 			<div className='inner'>
-				<div className='boardWrap'>
-					<div className='boardMo'>
-						<h1 className='contentsTitle'>Board</h1>
-						<div className='boardMoBtn'>
-							<Link className='boarLink' to='/board'>
-								view
-							</Link>
+				<article className='contentsHalf boardTitleCont'>
+					<div className='bContWrap'>
+						<div className='bTitleWrap'>
+							<div className='bBox'>
+								<h1 className='contentsTitle'>Board</h1>
+							</div>
+						</div>
+						<div className='bTextWrap'>
+							<div className='boardDescWrap'>
+								<p className='contentsSubTitle'> Write your comments for vogue</p>
+								<p className='boardDesc'>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe exercitationem,
+									harum ex laudantium quas nisi quae voluptatum repudiandae at quisquam maiores vero
+									itaque sapiente ut sint fugit, molestiae fugiat? Dolorem.
+								</p>
+							</div>
 						</div>
 					</div>
-					<div className='boardBox'>
-						<div className='boardTextArea'>
-							<p>Hello! This is Board Section</p>
-							<p>This is Board Section description</p>
-							<p>If you want to go to the board page,</p>
-							<p>
-								press the view button<span>|</span>
-							</p>
-						</div>
-						<div className='boardBtn'>
-							<Link className='boardBtnText' to='/board'>
-								view
-							</Link>
-						</div>
-					</div>
-				</div>
-				<div className='boardInfo'>
-					<div className='sectionAccodion'>
+				</article>
+				<article className='contentsHalf boardListCont'>
+					<ul className='boardLists'>
 						{Posts.map((post, idx) => {
 							if (idx >= 3) return null;
 							return (
-								<div className='tab' key={idx}>
-									<input type='radio' name='acc' id={idx} />
-									<label htmlFor={idx}>
-										<h2>0{idx + 1}</h2>
-										<h3>{post.title}</h3>
-									</label>
-									<div className='tabContents' key={idx}>
-										<p>{post.content}</p>
+								<li className='boardList' key={idx}>
+									<div className='boardBox'>
+										<p className='bListTitle'>{post.title}</p>
+										<p className='bListText'>{post.content}</p>
 									</div>
-								</div>
+								</li>
 							);
 						})}
-					</div>
-				</div>
+						<li className='boardList'>
+							<img src={process.env.PUBLIC_URL + '/img/dummy-gallery02.jpg'} alt='' />
+						</li>
+					</ul>
+				</article>
 			</div>
 		</section>
 	);
 }
+// <p>{post.content}</p>
 
 export default memo(BoardMain);
