a5ba6bec14ec9b8156f7384cb4b716cfdc7b278b
Shin-Yumi <tlsdbal0517@naver.com>
2023-03-20 12:48:28 +0900

    반응형 수정

--- src/components/common/CookiePopUp.js
+++ src/components/common/CookiePopUp.js
@@ -1,12 +1,31 @@
 import styled from 'styled-components';
 
+const Container = styled.div`
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: fixed;
+	top: 0;
+	left: 0;
+	z-index: 1000;
+`;
+
+const ModalBackground = styled.div`
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	z-index: 1000;
+`;
+
 const ModalContainer = styled.div`
 	position: fixed;
 	top: 100px;
 	right: 20px;
 	z-index: 1000;
-	width: 400px;
-	height: 300px;
+	width: 320px;
+	height: 250px;
 	padding: 30px;
 	background-color: #ecece3;
 	display: flex;
@@ -43,18 +62,19 @@ const ModalCloseWrapper = styled.div`
 
 function Cookie({ closeModal }) {
 	return (
-		<ModalContainer>
-			<ModalContent>
-				<p>
+		<Container>
+			<ModalBackground />
+			<ModalContainer>
+				<ModalContent>
 					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione labore sint impedit, optio mollitia quae soluta
 					possimus itaque obcaecati unde expedita laudantium ducimus distinctio quasi rerum molestiae velit, quibusdam cum.
-				</p>
-			</ModalContent>
-			<ModalCloseWrapper>
-				<p>오늘 하루 더 이상 보지 않기</p>
-				<p onClick={closeModal}>닫기</p>
-			</ModalCloseWrapper>
-		</ModalContainer>
+				</ModalContent>
+				<ModalCloseWrapper>
+					<p>오늘 하루 더 이상 보지 않기</p>
+					<p onClick={closeModal}>닫기</p>
+				</ModalCloseWrapper>
+			</ModalContainer>
+		</Container>
 	);
 }
 
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -49,7 +49,7 @@ function Header(props) {
 					<Link
 						ref={menu}
 						to='/'
-						className={menu.current.open === true ? 'hamMenu' : 'hamMenu on'}
+						className={menu.current.open === true ? 'hamMenu' : 'hamMenu'}
 						//className='hamMenu'
 						onClick={(e) => {
 							e.preventDefault();
